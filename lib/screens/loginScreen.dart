02af0ccf23df9b1edd0b2b20dbc4399612edc967
Vu Nguyen <vunguyen@Vus-MacBook-Pro.local>
2023-02-28 21:54:40 +0700

    project initialization

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:monkey_app_demo/screens/forgetPwScreen.dart';
+import 'package:monkey_app_demo/screens/homeScreen.dart';
 
 import '../const/colors.dart';
 import '../screens/forgetPwScreen.dart';
@@ -43,7 +44,10 @@ class LoginScreen extends StatelessWidget {
                   height: 50,
                   width: double.infinity,
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(
+                          context, HomeScreen.routeName);
+                    },
                     child: Text("Login"),
                   ),
                 ),
