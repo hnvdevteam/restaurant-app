f11c7557600023a5d5d16b88890053c7923ef00e
Vu Nguyen <nguyenquangvu1998@gmail.com>
2023-03-06 01:00:25 +0700

    update menuScreen|Unfinished

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: SplashScreen(),
+      home: MenuScreen(),
       routes: {
         LandingScreen.routeName: (context) => LandingScreen(),
         LoginScreen.routeName: (context) => LoginScreen(),
--- lib/screens/menuScreen.dart
+++ lib/screens/menuScreen.dart
@@ -57,22 +57,13 @@ class MenuScreen extends StatelessWidget {
                               color: AppColor.orange),
                         ),
                         Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               MenuCard(
-                                imageShape: ClipOval(
-                                  child: Container(
-                                    height: 60,
-                                    width: 60,
-                                    child: Image.asset(
-                                      Helper.getAssetName(
-                                          "western2.jpg", "real"),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
+                                image: "western2.jpg",
+                                file: "real",
                                 name: "Food",
                                 count: "120",
                               ),
@@ -80,18 +71,8 @@ class MenuScreen extends StatelessWidget {
                                 height: 20,
                               ),
                               MenuCard(
-                                imageShape: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: Container(
-                                    height: 60,
-                                    width: 60,
-                                    child: Image.asset(
-                                      Helper.getAssetName(
-                                          "coffee2.jpg", "real"),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
+                                image: "coffee2.jpg",
+                                file: "real",
                                 name: "Beverage",
                                 count: "220",
                               ),
@@ -104,18 +85,8 @@ class MenuScreen extends StatelessWidget {
                                       .pushNamed(DessertScreen.routeName);
                                 },
                                 child: MenuCard(
-                                  imageShape: ClipPath(
-                                    clipper: CustomTriangle(),
-                                    child: Container(
-                                      height: 70,
-                                      width: 70,
-                                      child: Image.asset(
-                                        Helper.getAssetName(
-                                            "dessert.jpg", "real"),
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  ),
+                                  image: "dessert.jpg",
+                                  file: "real",
                                   name: "Desserts",
                                   count: "135",
                                 ),
@@ -124,18 +95,8 @@ class MenuScreen extends StatelessWidget {
                                 height: 20,
                               ),
                               MenuCard(
-                                imageShape: ClipPath(
-                                  clipper: CustomDiamond(),
-                                  child: Container(
-                                    height: 80,
-                                    width: 80,
-                                    child: Image.asset(
-                                      Helper.getAssetName(
-                                          "hamburger3.jpg", "real"),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
+                                image: "hamburger3.jpg",
+                                file: "real",
                                 name: "Promotions",
                                 count: "25",
                               ),
@@ -160,21 +121,30 @@ class MenuScreen extends StatelessWidget {
   }
 }
 
-class MenuCard extends StatelessWidget {
+class MenuCard extends StatefulWidget {
   const MenuCard({
     Key key,
     @required String name,
     @required String count,
-    @required Widget imageShape,
+    @required String image,
+    @required String file,
   })  : _name = name,
         _count = count,
-        _imageShape = imageShape,
+        _image = image,
+        _file = file,
         super(key: key);
 
   final String _name;
   final String _count;
-  final Widget _imageShape;
+  final String _image;
+  final String _file;
 
+  @override
+  State<MenuCard> createState() => _MenuCardState();
+}
+
+class _MenuCardState extends State<MenuCard>
+    with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -183,15 +153,15 @@ class MenuCard extends StatelessWidget {
           height: 80,
           width: double.infinity,
           margin: const EdgeInsets.symmetric(
-            horizontal: 20,
+            horizontal: 10,
           ),
           padding: const EdgeInsets.symmetric(
-            horizontal: 80,
+            horizontal: 10,
           ),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(50),
-              bottomLeft: Radius.circular(50),
+              topLeft: Radius.circular(10),
+              bottomLeft: Radius.circular(10),
               topRight: Radius.circular(10),
               bottomRight: Radius.circular(10),
             ),
@@ -204,134 +174,76 @@ class MenuCard extends StatelessWidget {
               ),
             ],
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Row(
             children: [
-              Text(
-                _name,
-                style: Helper.getTheme(context).headline4.copyWith(
-                      color: AppColor.primary,
+              Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    height: 60,
+                    width: 60,
+                    child: Image.asset(
+                      Helper.getAssetName(widget._image, widget._file),
+                      fit: BoxFit.cover,
                     ),
+                  ),
+                ),
               ),
-              SizedBox(
-                height: 5,
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(left: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget._name,
+                        style: Helper.getTheme(context).headline4.copyWith(
+                              color: AppColor.primary,
+                            ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text("${widget._count} items")
+                    ],
+                  ),
+                ),
               ),
-              Text("$_count items")
+              Container(
+                height: 80,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    decoration: ShapeDecoration(
+                        shape: CircleBorder(),
+                        color: Colors.white,
+                        shadows: [
+                          BoxShadow(
+                            color: AppColor.placeholder,
+                            offset: Offset(0, 2),
+                            blurRadius: 5,
+                          )
+                        ]),
+                    child: Image.asset(
+                      Helper.getAssetName("next_filled.png", "virtual"),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
-        SizedBox(
-          height: 80,
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: _imageShape,
-          ),
-        ),
-        SizedBox(
-          height: 80,
-          child: Align(
-            alignment: Alignment.centerRight,
-            child: Container(
-              height: 40,
-              width: 40,
-              decoration: ShapeDecoration(
-                  shape: CircleBorder(),
-                  color: Colors.white,
-                  shadows: [
-                    BoxShadow(
-                      color: AppColor.placeholder,
-                      offset: Offset(0, 2),
-                      blurRadius: 5,
-                    )
-                  ]),
-              child: Image.asset(
-                Helper.getAssetName("next_filled.png", "virtual"),
-              ),
-            ),
-          ),
-        ),
+        // SizedBox(
+        //   height: 80,
+        //   child: Align(
+        //     alignment: Alignment.centerLeft,
+        //     child: _image,
+        //   ),
+        // ),
       ],
     );
-  }
-}
-
-class CustomTriangle extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    Offset controlpoint = Offset(size.width * 0, size.height * 0.5);
-    Offset endpoint = Offset(size.width * 0.2, size.height * 0.85);
-    Offset controlpoint2 = Offset(size.width * 0.33, size.height);
-    Offset endpoint2 = Offset(size.width * 0.6, size.height * 0.9);
-    Offset controlpoint3 = Offset(size.width * 1.4, size.height * 0.5);
-    Offset endpoint3 = Offset(size.width * 0.6, size.height * 0.1);
-    Offset controlpoint4 = Offset(size.width * 0.33, size.height * 0);
-    Offset endpoint4 = Offset(size.width * 0.2, size.height * 0.15);
-
-    Path path = new Path()
-      ..moveTo(size.width * 0.2, size.height * 0.15)
-      ..quadraticBezierTo(
-        controlpoint.dx,
-        controlpoint.dy,
-        endpoint.dx,
-        endpoint.dy,
-      )
-      ..quadraticBezierTo(
-        controlpoint2.dx,
-        controlpoint2.dy,
-        endpoint2.dx,
-        endpoint2.dy,
-      )
-      ..quadraticBezierTo(
-        controlpoint3.dx,
-        controlpoint3.dy,
-        endpoint3.dx,
-        endpoint3.dy,
-      )
-      ..quadraticBezierTo(
-        controlpoint4.dx,
-        controlpoint4.dy,
-        endpoint4.dx,
-        endpoint4.dy,
-      );
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return true;
-  }
-}
-
-class CustomDiamond extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    Path path = Path();
-    path.moveTo(size.width * 0.1000000, size.height * 0.4400000);
-    path.quadraticBezierTo(size.width * 0.0243800, size.height * 0.5247000,
-        size.width * 0.1000000, size.height * 0.6000000);
-    path.quadraticBezierTo(size.width * 0.3550000, size.height * 0.8250000,
-        size.width * 0.4400000, size.height * 0.9000000);
-    path.quadraticBezierTo(size.width * 0.5140600, size.height * 0.9574800,
-        size.width * 0.5800000, size.height * 0.9000000);
-    path.quadraticBezierTo(size.width * 0.8200000, size.height * 0.6450000,
-        size.width * 0.9000000, size.height * 0.5600000);
-    path.quadraticBezierTo(size.width * 0.9500400, size.height * 0.5009400,
-        size.width * 0.9000000, size.height * 0.4200000);
-    path.quadraticBezierTo(size.width * 0.6450000, size.height * 0.1800000,
-        size.width * 0.5600000, size.height * 0.1000000);
-    path.quadraticBezierTo(size.width * 0.5029400, size.height * 0.0446800,
-        size.width * 0.4200000, size.height * 0.1000000);
-    path.quadraticBezierTo(size.width * 0.3400000, size.height * 0.1850000,
-        size.width * 0.1000000, size.height * 0.4400000);
-    path.close();
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return true;
   }
 }
