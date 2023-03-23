537e9b91efbdb790602b2555e8c7b6611c5bdd35
Nguyễn Thị Kim Ngọc <ngocntk996@gmail.com>
2023-03-23 15:57:25 +0700

    feat: connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:monkey_app_demo/const/colors.dart';
 import 'package:monkey_app_demo/screens/aboutScreen.dart';
@@ -23,7 +24,13 @@ import 'package:monkey_app_demo/screens/sentOTPScreen.dart';
 import 'package:monkey_app_demo/screens/signUpScreen.dart';
 import 'package:monkey_app_demo/widgets/layout.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
