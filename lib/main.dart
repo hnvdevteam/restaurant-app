13d7ede8c38bec6c35971e3b9e00577268fc7ea5
Nguyễn Thị Kim Ngọc <ngocntk996@gmail.com>
2023-03-27 13:28:36 +0700

    clean: theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:monkey_app_demo/const/colors.dart';
 import 'package:monkey_app_demo/routes.dart';
+import 'package:monkey_app_demo/theme.dart';
 import 'package:monkey_app_demo/widgets/layout.dart';
 
 import 'firebase_options.dart';
@@ -20,52 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: "Metropolis",
-        primarySwatch: Colors.red,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(
-              AppColor.orange,
-            ),
-            shape: MaterialStateProperty.all(
-              StadiumBorder(),
-            ),
-            elevation: MaterialStateProperty.all(0),
-          ),
-        ),
-        textButtonTheme: TextButtonThemeData(
-          style: ButtonStyle(
-            foregroundColor: MaterialStateProperty.all(
-              AppColor.orange,
-            ),
-          ),
-        ),
-        textTheme: TextTheme(
-          headline3: TextStyle(
-            color: AppColor.primary,
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-          headline4: TextStyle(
-            color: AppColor.secondary,
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-          headline5: TextStyle(
-            color: AppColor.primary,
-            fontWeight: FontWeight.normal,
-            fontSize: 25,
-          ),
-          headline6: TextStyle(
-            color: AppColor.primary,
-            fontSize: 25,
-          ),
-          bodyText2: TextStyle(
-            color: AppColor.secondary,
-          ),
-        ),
-      ),
+      theme: themeData,
       home: LayoutScreen(0),
       routes: AppRoutes.routes,
     );
---
+++ lib/theme.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:monkey_app_demo/const/colors.dart';
+
+final ThemeData themeData = ThemeData(
+  fontFamily: "Metropolis",
+  primarySwatch: Colors.red,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(
+        AppColor.orange,
+      ),
+      shape: MaterialStateProperty.all(
+        StadiumBorder(),
+      ),
+      elevation: MaterialStateProperty.all(0),
+    ),
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: ButtonStyle(
+      foregroundColor: MaterialStateProperty.all(
+        AppColor.orange,
+      ),
+    ),
+  ),
+  textTheme: TextTheme(
+    headline3: TextStyle(
+      color: AppColor.primary,
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+    ),
+    headline4: TextStyle(
+      color: AppColor.secondary,
+      fontWeight: FontWeight.bold,
+      fontSize: 20,
+    ),
+    headline5: TextStyle(
+      color: AppColor.primary,
+      fontWeight: FontWeight.normal,
+      fontSize: 25,
+    ),
+    headline6: TextStyle(
+      color: AppColor.primary,
+      fontSize: 25,
+    ),
+    bodyText2: TextStyle(
+      color: AppColor.secondary,
+    ),
+  ),
+);
